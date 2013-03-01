4d19a07dfbdae8937acefa39a4eb4aaa51b27ced
RolaJeffrey <jeffrey410469@gmail.com>
2013-03-01 14:40:47 +0800

    1.From now on, the users can register & login in the orbit.html 2.Users can link to orbit from newindex.html now

--- app/controllers/newindex_controller.rb
+++ app/controllers/newindex_controller.rb
@@ -13,6 +13,20 @@ class NewindexController < ApplicationController
 		end
 	end
 	
+	def orbit
+		if params[:id]
+			redirect_to '/rapid/index/'+params[:id]
+		end		
+
+		if UserSession.find
+			redirect_to '/rapid/index'
+		else		
+			logPosition '/'
+			@newuser=User.new
+			@user_session=UserSession.new
+		end
+	end
+
 	def logPosition str
 		session[:url]=str
 	end	
