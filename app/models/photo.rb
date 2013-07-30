ebb3718974e9c9b28819a11face2c7bce7bbdac4
BertHuang <berberhuang@gmail.com>
2013-07-30 16:15:32 +0800

    auto rotate on server

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,21 +7,24 @@ class Photo < ActiveRecord::Base
 	:path => "photos/:photos_trip_id/:style/:id:basename.:extension",
 	:storage=>:s3,
 	:bucket=>'wwtest-photo',
-	:styles=>{:thumb=>"200x200"}
+	:styles=>{:thumb=>"200x200",:original=>"1917x1917"},
+	:source_file_options=>{:all=>'-auto-orient'}
 	
 
 	validates_attachment_presence :img
 	validates_attachment_content_type :img, :content_type=>['image/jpeg','image/png']	
 
 
+
+
 	def to_jq_upload
-	{
+		{
 		"name" => read_attribute(:img_file_name),
 		"size" => read_attribute(:img_file_size),
 		"url" => img.url(:thumb),
 		"original"=>img.url(:original)
 		#"delete_url" => file.path(self),
 		#"delete_type" => "DELETE" 
-	}
+		}
 	end
 end
